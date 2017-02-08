5dcd32618e4043b5598e5ff2cd5438a17f02be3c
Valérian Galliat <val@codejam.info>
2017-02-08 16:11:08 -0500

    Style updates

--- node-fetch.js
+++ node-fetch.js
@@ -4,17 +4,19 @@ module.exports = function nodeFetchCookieDecorator (nodeFetch, jar) {
   return function nodeFetchCookie (url, opts) {
     // Forward identical options to wrapped node-fetch but tell to not handle redirection.
     return fetchCookie(url, Object.assign({}, opts, { redirect: 'manual' }))
-      .then(function (res) {
+      .then(res => {
         var isRedirect = (res.status === 303 || ((res.status === 301 || res.status === 302) && opts.method === 'POST'))
-        // interpret the proprietary "redirect" option in the same way that node-fetch does
+
+        // Interpret the proprietary "redirect" option in the same way that node-fetch does.
         if (isRedirect && opts.redirect !== 'manual' && opts.follow !== 0) {
           var optsForGet = Object.assign({}, {
             method: 'GET',
             body: null,
-            // since the "follow" flag is not relevant for node-fetch in this case,
-            // we'll hijack it for our internal bookkeeping
+            // Since the "follow" flag is not relevant for node-fetch in this case,
+            // we'll hijack it for our internal bookkeeping.
             follow: opts.follow !== undefined ? opts.follow - 1 : undefined
           })
+
           return nodeFetchCookie(res.headers.get('location'), optsForGet)
         } else {
           return res
