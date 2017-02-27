5f38a17aa8d276949c2d472157bf9c9f03ba0146
Gajus Kuizinas <gajus@gajus.com>
2017-02-27 15:14:54 -0500

    Fix node-fetch redirect implementation
    Closes #13.

--- node-fetch.js
+++ node-fetch.js
@@ -1,22 +1,22 @@
 module.exports = function nodeFetchCookieDecorator (nodeFetch, jar) {
   var fetchCookie = require('./')(nodeFetch, jar)
 
-  return function nodeFetchCookie (url, opts) {
-    opts = Object.assign({}, opts, { redirect: 'manual' })
+  return function nodeFetchCookie (url, userOptions) {
+    const opts = Object.assign({}, userOptions, { redirect: 'manual' })
 
     // Forward identical options to wrapped node-fetch but tell to not handle redirection.
     return fetchCookie(url, opts)
       .then(res => {
-        var isRedirect = (res.status === 303 || ((res.status === 301 || res.status === 302) && opts.method === 'POST'))
+        var isRedirect = (res.status === 303 || ((res.status === 301 || res.status === 302)))
 
         // Interpret the proprietary "redirect" option in the same way that node-fetch does.
-        if (isRedirect && opts.redirect !== 'manual' && opts.follow !== 0) {
+        if (isRedirect && userOptions.redirect !== 'manual' && userOptions.follow !== 0) {
           var optsForGet = Object.assign({}, {
             method: 'GET',
             body: null,
             // Since the "follow" flag is not relevant for node-fetch in this case,
             // we'll hijack it for our internal bookkeeping.
-            follow: opts.follow !== undefined ? opts.follow - 1 : undefined
+            follow: userOptions.follow !== undefined ? userOptions.follow - 1 : undefined
           })
 
           return nodeFetchCookie(res.headers.get('location'), optsForGet)
