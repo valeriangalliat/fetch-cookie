1aa3643d4054b541aae2e99fb6ac864b2be65ad0
Alexis Tyler <xo@wvvw.me>
2018-02-01 12:30:47 +1030

    add cookiejar test

--- test.js
+++ test.js
@@ -12,6 +12,11 @@ app.get('/set', (req, res) => {
   res.end()
 })
 
+app.get('/set2', (req, res) => {
+  res.set('set-cookie', 'foo=bar2')
+  res.end()
+})
+
 app.get('/get', (req, res) => {
   res.end(req.headers.cookie)
 })
@@ -23,12 +28,24 @@ app.get('/redirect', (req, res) => {
 app.listen(9999)
 
 describe('fetch-cookie', () => {
-  it('should handle cookies', () => {
-    return fetch('http://localhost:9999/set')
-      .then(() => fetch('http://localhost:9999/get'))
-      .then(res => res.text())
-      .then(body => {
-        equal(body, 'foo=bar')
-      })
+  it('should handle cookies', async () => {
+    await fetch('http://localhost:9999/set');
+    const res = await fetch('http://localhost:9999/get');
+    equal(await res.text(), 'foo=bar');
+  })
+  it('should handle cookies jars', async () => {
+    const cookieJar1 = {};
+    const cookieJar2 = {};
+    await fetch('http://localhost:9999/set', cookieJar1);
+    const res1 = await fetch('http://localhost:9999/get', cookieJar1);
+
+    await fetch('http://localhost:9999/set2', cookieJar2);
+    const res2 = await fetch('http://localhost:9999/get', cookieJar2);
+
+    equal(await res1.text(), 'foo=bar');
+    equal(cookieJar1.headers.cookie, 'foo=bar');
+
+    equal(await res2.text(), 'foo=bar2');
+    equal(cookieJar2.headers.cookie, 'foo=bar2');
   })
 })
