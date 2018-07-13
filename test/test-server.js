3fad9a3f27f5e9a6802b9b99fd723704499e9167
Fabian Te <fabiante@users.noreply.github.com>
2018-07-13 10:12:00 +0200

    Improved tests
    - Split tests into a test file and a file containing the test server
    - Created before and after mocha hooks that start and stop the test server.

---
+++ test/test-server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+app.use(cookieParser())
+
+// Respons with a 'set-cookie' header with a single cookie with only a name and value created by the URL params
+app.get('/set', (req, res) => {
+  const { name, value } = req.query
+  res.set('set-cookie', `${name}=${value}`)
+  res.end()
+})
+
+app.get('/set-multiple', (req, res) => {
+  res.append('set-cookie', 'foo=bat; expires=Mon, 17-Jul-2017 16:06:00 GMT; Max-Age=31449600; Path=/; secure')
+  res.append('set-cookie', 'tuna=can')
+  res.end()
+})
+
+app.get('/get', (req, res) => {
+  res.end(req.headers.cookie)
+})
+
+app.get('/redirect', (req, res) => {
+  res.redirect('http://localhost:9998/get') // FIXME: There is nothing at this port ...
+})
+
+module.exports = app
--- test/test.js
+++ test/test.js
@@ -1,45 +1,37 @@
 /* eslint-env mocha */
 
 const { equal } = require('assert')
-const express = require('express')
 const nodeFetch = require('node-fetch')
 const fetch = require('../')(nodeFetch)
-
-const app = express()
-
-app.get('/set', (req, res) => {
-  res.set('set-cookie', 'foo=bar')
-  res.end()
-})
-
-app.get('/set2', (req, res) => {
-  res.set('set-cookie', 'foo=bar2')
-  res.end()
-})
-
-app.get('/get', (req, res) => {
-  res.end(req.headers.cookie)
-})
-
-app.get('/redirect', (req, res) => {
-  res.redirect('http://localhost:9998/get')
-})
-
-app.listen(9999)
+const app = require('./test-server');
 
 describe('fetch-cookie', () => {
+  let server
+
+  before('start test server', async () => {
+    return new Promise((resolve, reject) => {
+      server = app.listen(9999, (err) => {
+        if (err)
+          reject(err);
+        else
+          resolve();
+      })
+    })
+  })
+
   it('should handle cookies', async () => {
-    await fetch('http://localhost:9999/set');
+    await fetch('http://localhost:9999/set?name=foo&value=bar');
     const res = await fetch('http://localhost:9999/get');
     equal(await res.text(), 'foo=bar');
   })
+
   it('should handle cookies jars', async () => {
     const cookieJar1 = {};
     const cookieJar2 = {};
-    await fetch('http://localhost:9999/set', cookieJar1);
+    await fetch('http://localhost:9999/set?name=foo&value=bar', cookieJar1);
     const res1 = await fetch('http://localhost:9999/get', cookieJar1);
 
-    await fetch('http://localhost:9999/set2', cookieJar2);
+    await fetch('http://localhost:9999/set?name=foo&value=bar2', cookieJar2);
     const res2 = await fetch('http://localhost:9999/get', cookieJar2);
 
     equal(await res1.text(), 'foo=bar');
@@ -47,5 +39,10 @@ describe('fetch-cookie', () => {
 
     equal(await res2.text(), 'foo=bar2');
     equal(cookieJar2.headers.cookie, 'foo=bar2');
+  })
+
+  after('stop test server', () => {
+    if (server)
+      server.close();
   })
 })
